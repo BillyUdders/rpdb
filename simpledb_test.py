7b81d1fa794d999b779a37a03cce0b1905016f71
Rhysyrhysrhys <rhys@golden.co>
2021-12-22 06:13:06 -0800

    Cleaning up test code

--- simpledb_test.py
+++ simpledb_test.py
@@ -1,50 +1,65 @@
 import unittest
-import main
+from main import SimpleDB, Operation, OperationType
 
 
 class MyTestCase(unittest.TestCase):
-    def test_set_get_unset(self):
-        db = main.SimpleDB()
+    def test_set_get_unset_exists(self):
+        db = SimpleDB()
 
         db.set("a", 12)
         self.assertEqual(db.get("a"), 12)
+        self.assertTrue(db.exists("a"))
 
         db.unset("a")
         self.assertRaises(KeyError, db.get, "a")
         self.assertFalse(db.exists("a"))
 
+    def test_transaction_log(self):
+        db = SimpleDB()
+
+        with db.transaction():
+            db.set("a", 12)
+            db.set("b", 23)
+            db.unset("b")
+            self.assertEqual(len(db.live_transactions), 1)
+            operation_captor = db.live_transactions[-1].operations
+
+        self.assertEqual(
+            operation_captor,
+            [
+                Operation(OperationType.BEGIN, None, None),
+                Operation(OperationType.SET, "a", 12),
+                Operation(OperationType.SET, "b", 23),
+                Operation(OperationType.UNSET, "b", None),
+                Operation(OperationType.COMMIT, None, None),
+            ],
+        )
+        self.assertEqual(len(db.live_transactions), 0)
+
+    def test_transaction_nesting(self):
+        db = SimpleDB()
+        with db.transaction():
+            with db.transaction():
+                self.assertEqual(len(db.live_transactions), 2)
+            self.assertEqual(len(db.live_transactions), 1)
+        self.assertEqual(len(db.live_transactions), 0)
+
     def test_tx_set_rollback(self):
-        db = main.SimpleDB()
+        db = SimpleDB()
 
         db.set("a", 12)
         db.set("b", 23)
         self.assertEqual(db.get("a"), 12)
         self.assertEqual(db.get("b"), 23)
 
-        with db.transaction() as tx:
+        with db.transaction():
             db.set("a", 49)
             db.unset("b")
-            # self.assertEqual(db.get("a"), 49)
-            # self.assertFalse(db.exists("b"))
+            self.assertEqual(db.get("a"), 49)
+            self.assertFalse(db.exists("b"))
 
-            # We're in a transaction
-            self.assertEqual(len(db.live_transactions), 1)
-            # BEGIN, SET, UNSET
-            self.assertEqual(len(db.live_transactions[-1].operations), 3)
-
-            with db.transaction() as tx2:
-                # Nested transaction
-                self.assertEqual(len(db.live_transactions), 2)
-                tx2.rollback()
-
-            # Nested transaction committed
-            self.assertEqual(len(db.live_transactions), 1)
-            tx.rollback()
-
-        self.assertEqual(db.get("a"), 12)
+        self.assertEqual(db.get("a"), 49)
         self.assertEqual(db.get("b"), 23)
-        self.assertListEqual(db.live_transactions, [])
-        self.assertEqual(len(db.transaction_log), 4)
 
 
 if __name__ == "__main__":
