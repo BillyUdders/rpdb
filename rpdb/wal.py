47fd01f8ae66cb8e75f1ec92ab498e4181be70ac
Rhysyrhysrhys <rhys@golden.co>
2021-12-28 10:51:49 -0800

    Adding protobuf getting the tests to pass

--- rpdb/wal.py
+++ rpdb/wal.py
@@ -1,46 +1,29 @@
-import json
 import time
-from struct import pack, unpack
 from typing import Iterator, Tuple
 
+from proto.WAL_pb2 import WAL as Store
+from proto.WAL_pb2 import WALEntry as Entry
 from rpdb.operations import Operation
 
 
 class WAL:
     def __init__(self, wal_file_location: str) -> None:
         self.writer = open(wal_file_location, "+ab")
+        self.store = Store()
 
     def __del__(self):
         self.writer.close()
 
     def append(self, op: Operation):
-        key_bytes = self.__create_key()
-        value_bytes = json.dumps(op).encode("utf-8")
-
-        self.writer.write(pack("I", len(key_bytes)))
-        self.writer.write(key_bytes)
-        self.writer.write(pack("I", len(value_bytes)))
-        self.writer.write(value_bytes)
-        self.writer.flush()
+        entry = Entry()
+        entry.timestamp = time.time_ns()
 
     def restore_all(self) -> Iterator[Tuple[str, str]]:
         self.writer.seek(0)
         yield from self._read_pairs()
 
     def _read_pairs(self) -> Iterator[Tuple[str, str]]:
-        while True:
-            key_len_bytes = self.writer.read(len(self.__create_key()))
-            if not key_len_bytes:
-                break
-            (key_len,) = unpack("I", key_len_bytes)
-            key_bytes = self.writer.read(key_len)
-            (value_len,) = unpack("I", self.writer.read(4))
-            value_bytes = self.writer.read(value_len)
-            yield key_bytes.decode("utf-8"), value_bytes.decode("utf-8")
+        pass
 
     def clear(self):
         self.writer.truncate(0)
-
-    @staticmethod
-    def __create_key():
-        return str(time.time_ns()).encode("utf-8")
