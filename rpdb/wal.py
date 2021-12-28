2d6440029cc68d370db3681f6d931459180983c5
Rhysyrhysrhys <rhys@golden.co>
2021-12-28 12:01:39 -0800

    Mypy and protobuf really don't like eachother wtf

--- rpdb/wal.py
+++ rpdb/wal.py
@@ -1,22 +1,39 @@
 import time
 from typing import Iterator, Tuple
 
-from proto.WAL_pb2 import WAL as Store
-from proto.WAL_pb2 import WALEntry as Entry
-from rpdb.operations import Operation
+from proto.WAL_pb2 import WAL, WALEntry
+from rpdb.operations import Operation, WriterOps
 
 
-class WAL:
+class WriteAheadLog:
     def __init__(self, wal_file_location: str) -> None:
         self.writer = open(wal_file_location, "+ab")
-        self.store = Store()
+        self.store = WAL()
 
     def __del__(self):
         self.writer.close()
 
     def append(self, op: Operation):
-        entry = Entry()
+        self.__create_entry(self.store.add(), op)
+        self.writer.write(self.store.SerializeToString())
+        self.writer.flush()
+
+    @staticmethod
+    def __create_entry(entry, op: Operation):
         entry.timestamp = time.time_ns()
+        entry.key = op.key
+        entry.value = op.value
+
+        if op.op_type == WriterOps.BEGIN:
+            entry.op_type = WALEntry.OpType.BEGIN  # type: ignore
+        if op.op_type == WriterOps.SET:
+            entry.op_type = WALEntry.OpType.SET  # type: ignore
+        if op.op_type == WriterOps.UNSET:
+            entry.op_type = WALEntry.OpType.UNSET  # type: ignore
+        if op.op_type == WriterOps.COMMIT:
+            entry.op_type = WALEntry.OpType.COMMIT  # type: ignore
+        if op.op_type == WriterOps.ROLLBACK:
+            entry.op_type = WALEntry.OpType.ROLLBACK  # type: ignore
 
     def restore_all(self) -> Iterator[Tuple[str, str]]:
         self.writer.seek(0)
---
+++ tests/wal_test.py
@@ -0,0 +1,5 @@
+import unittest
+
+
+class WALTestCase(unittest.TestCase):
+    pass
